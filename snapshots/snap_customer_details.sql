a9b3b68c03e385d3d27dd4289b91c32fb9b92340
dbt user <dakshinasuresh22@gmail.com>
2026-05-17 06:48:48 +0000

    .

--- snapshots/snap_customer_details.sql
+++ snapshots/snap_customer_details.sql
@@ -5,7 +5,7 @@
     (
         unique_key='customer_id',
         strategy='check',
-        check_cols=['']
+        check_cols=['email','first_name','last_name','country','state','city']
     )
 }}
 
