9885bbc85e5236b4ae99b331f4044f90acffaba1
dbt user <dakshinasuresh22@gmail.com>
2026-05-17 06:47:55 +0000

    Update

---
+++ snapshots/snap_customer_phn.sql
@@ -0,0 +1,26 @@
+{%snapshot snap_customer_phn%}
+
+{{
+    config
+    (
+        unique_key='customer_id',
+        strategy='check',
+        check_cols=['']
+    )
+}}
+
+
+with de_dupliate_customer as
+(
+
+    select customer_id,email,first_name,last_name,country,state,city,time 
+    from  {{ ref('customers_stg_dak') }} 
+    qualify row_number() over(partition by customer_id order by time desc)=1
+
+)
+
+select * from de_dupliate_customer
+
+
+
+{%endsnapshot%}
