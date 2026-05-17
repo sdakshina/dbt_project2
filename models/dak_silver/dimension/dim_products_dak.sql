0a84122195896f3f2363973e63fd1298c925e034
dbt user <dakshinasuresh22@gmail.com>
2026-05-17 10:02:05 +0000

    Update

---
+++ models/dak_silver/dimension/dim_products_dak.sql
@@ -0,0 +1,20 @@
+{{
+    config
+    {
+        materialized='incrementa'
+    }
+}}
+
+
+select  {{ 
+        dbt_utils.generate_surrogate_key(['product_id', 'dbt_valid_from'])
+    }} as product_skey,
+    product_id,product_name,price,main_category,sub_category,supplier_id,loaded_time,
+    DBT_VALID_FROM as start_time,
+    DBT_VALID_TO end_time,
+    case when DBT_VALID_TO is null then 'Y' else 'N' END active
+     from {{ ref('snap_products') }} src
+
+{%is is_incremental()%}
+where src.loaded_time >(select max(loaded_time)) from {{this}}
+{%endif%}
