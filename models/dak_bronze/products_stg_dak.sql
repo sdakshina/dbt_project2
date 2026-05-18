c5f8bd876150ad1b776afa27e7a8cdabb8868424
dbt user <dakshinasuresh22@gmail.com>
2026-05-18 17:39:48 +0000

    Update

--- models/dak_bronze/products_stg_dak.sql
+++ models/dak_bronze/products_stg_dak.sql
@@ -1,4 +1,5 @@
 SELECT
+distinct
     a.VAR_DATA:product_id::STRING AS product_id,
     a.VAR_DATA:product_name::STRING AS product_name,
     a.VAR_DATA:price::NUMBER AS price,
@@ -6,7 +7,7 @@ SELECT
     a.VAR_DATA:category.sub::STRING AS sub_category,
     a.VAR_DATA:supplier.supplier_id::STRING AS supplier_id,
     a.VAR_DATA:supplier.supplier_name::STRING AS supplier_name,
-    f.VALUE::STRING AS tag,
+    array_to_string(a.var_data:tags,',') as tags,
     current_timestamp() as loaded_time
 FROM {{source('bronze','PRODUCTS_RAW_DAK')}} a,
 LATERAL FLATTEN(input => a.VAR_DATA:tags) f
--- models/dak_silver/fact/fact_orders.sql
+++ models/dak_silver/fact/fact_orders.sql
@@ -3,15 +3,15 @@
 }}
 
 
-select
-{{ dbt_utils.generate_surrogate_key([
+select distinct
+ {{ dbt_utils.generate_surrogate_key([
 'o.order_id',
 'o.product_id'
 ]) }} as order_fact_sk,
 o.order_id,
 c.customer_skey,
 p.product_skey,
-pm.payment_method_sk,
+-- pm.payment_method_sk,
 s.shipment_sk,
 to_number(to_char(o.order_date,'YYYYMMDD')) as order_date_sk,
 o.quantity,
@@ -28,10 +28,10 @@ on o.customer_id = c.customer_id and c.active='Y'
 and o.order_date >= to_CHAR(c.start_time,'YYYY-MM-DD')
 and o.order_date < to_date(coalesce(c.end_time,'9999-12-31'))
 left join {{ ref('dim_products_dak') }} p
-on o.product_id = p.product_id and p.active='Y'
+on o.product_id = p.product_id and p.active='Y' 
 and o.order_date >= to_char(p.start_time,'YYYY-MM-DD')
 and o.order_date < to_date(coalesce(P.end_time,'9999-12-31'))
-left join {{ ref('dim_payments') }} pm
-on o.method = pm.payment_method
+-- left join {{ ref('dim_payments') }} pm
+-- on o.ORDER_ID = pm.payment_method
 left join {{ ref('dim_shipments') }} s
 on o.order_id = s.order_id
