c67f0701e20f277be15263dfa2efb9ca78547659
dbt user <dakshinasuresh22@gmail.com>
2026-05-20 09:44:13 +0000

    Updating Gold layer

--- models/dak_gold/mart_sales_summary.sql
+++ models/dak_gold/mart_sales_summary.sql
@@ -3,10 +3,10 @@ select
 d.full_date,
 p.main_category,
 count(distinct f.order_id) as total_orders,
-sum(f.item_quantity) as total_quantity,
+sum(f.quantity) as total_quantity,
 sum(f.net_amount) as total_sales,
 avg(f.net_amount) as avg_order_value,
-count(distinct f.cust_key) as total_customers
+count(distinct f.customer_skey) as total_customers
 from {{ ref('fact_orders')}} f
 left join {{ ref('dim_products_dak')}} p
 on f.product_skey = p.product_skey
