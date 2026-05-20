055738759b55aab3ff06237a148bc85373128333
dbt user <dakshinasuresh22@gmail.com>
2026-05-20 09:30:10 +0000

    Update Gold layer dakshina models

--- models/dak_gold/mart_sales_summary.sql
+++ models/dak_gold/mart_sales_summary.sql
@@ -7,9 +7,9 @@ sum(f.item_quantity) as total_quantity,
 sum(f.net_amount) as total_sales,
 avg(f.net_amount) as avg_order_value,
 count(distinct f.cust_key) as total_customers
-from {{ ref('fact_order_sara')}} f
-left join {{ ref('dim_product_sara')}} p
+from {{ ref('fact_orders')}} f
+left join {{ ref('dim_products_dak')}} p
 on f.product_skey = p.product_skey
-left join {{ ref('dim_date_sara')}} d
+left join {{ ref('dim_date')}} d
 on f.order_date_sk = d.date_sk
 group by 1,2
